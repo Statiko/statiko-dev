8cdfa1a0e598e5798318dbde6d9407fce491dc39
Piyush Agrawal <poush12@gmail.com>
2016-06-03 18:35:50 +0530

    Initial Commit => Working on  Prototype

---
+++ app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component }       from '@angular/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+
+import { HomeComponent } from './components/home/home.component';
+
+import { AppConfig } from './config/app';
+
+
+@Component({
+	selector: 'my-app',
+	template: `
+    <h1>{{title}}</h1>
+    <router-outlet></router-outlet> 
+   `,
+
+	directives: [ROUTER_DIRECTIVES],
+	providers: [
+		ROUTER_PROVIDERS,
+		AppConfig
+	]
+})
+
+@RouteConfig([
+	{
+		path: '/',
+		name: 'Home',
+		component: HomeComponent
+	}
+])
+
+export class AppComponent {
+	title = AppConfig.title;
+}
---
+++ app/components/home/home.component.ts
@@ -0,0 +1,7 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+	selector: 'content',
+})
+export class HomeComponent {
+}
